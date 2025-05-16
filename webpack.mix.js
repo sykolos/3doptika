321635558351c378205fd8154e5fbcb0d01af7d1
Sivatagi Sas <sykolos6@gmail.com>
2025-05-16 16:57:43 +0200

    webpack csere

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,21 +2,22 @@ const mix = require('laravel-mix');
 const fs = require('fs-extra');
 require('laravel-mix-jigsaw');
 
-
 mix.disableSuccessNotifications();
-mix.setPublicPath('source/assets/build');
+mix.setPublicPath('source/assets');
 
 mix.jigsaw()
-    .sass('source/_assets/css/main.sass', 'css')
-    .js('source/_assets/js/main.js', 'js')
+    .sass('source/_assets/css/main.sass', 'build/css')
+    .js('source/_assets/js/main.js', 'build/js')
     .options({
         processCssUrls: false,
     })
     .version();
+
+// Képek másolása
 mix.then(() => {
-        const srcDir = 'source/_assets/img';
-        const destDir = 'source/assets/img';
-        if (fs.existsSync(srcDir)) {
-            fs.copySync(srcDir, destDir);
-        }
+    const srcDir = 'source/_assets/img';
+    const destDir = 'source/assets/img';
+    if (fs.existsSync(srcDir)) {
+        fs.copySync(srcDir, destDir);
+    }
 });
