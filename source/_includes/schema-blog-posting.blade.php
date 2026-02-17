10a420bb67db8934e0acdb637a041b1227749e8e
Sivatagi Sas <sykolos6@gmail.com>
2026-02-17 14:52:40 +0100

    tesztnek

--- source/_includes/schema-blog-posting.blade.php
+++ source/_includes/schema-blog-posting.blade.php
@@ -1,35 +1,24 @@
-@if (isset($page->author) && isset($page->date))
 <script type="application/ld+json">
-@php
-  $schema = [
-    "@context" => "https://schema.org",
-    "@type" => "BlogPosting",
-    "mainEntityOfPage" => [
-      "@type" => "WebPage",
-      "@id" => url($page->getUrl())
-    ],
-    "headline" => strip_tags($page->title),
-    "datePublished" => \Carbon\Carbon::parse($page->date)->toIso8601String(),
-    "dateModified" => \Carbon\Carbon::parse($page->date)->toIso8601String(),
-    "author" => [
-      "@type" => "Person",
-      "name" => $page->author
-    ],
-    "publisher" => [
-      "@type" => "Organization",
-      "name" => "3D Optika",
-      "logo" => [
-        "@type" => "ImageObject",
-        "url" => url('/assets/images/logo.png')
-      ]
-    ]
-  ];
-
-  if (!empty($page->featured_image)) {
-    $schema["image"] = [$page->featured_image];
+{
+  "@context": "https://schema.org",
+  "@type": "BlogPosting",
+  "mainEntityOfPage": {
+    "@type": "WebPage",
+    "@id": "{{ $page->getUrl() }}"
+  },
+  "headline": "{{ strip_tags($page->title) }}",
+  "datePublished": "{{ date('c', $page->date) }}",
+  "dateModified": "{{ date('c', $page->date) }}"
+  @if ($page->featured_image),
+  "image": ["{{ $page->featured_image }}"]
+  @endif,
+  "author": {
+    "@type": "Person",
+    "name": "{{ $page->author }}"
+  },
+  "publisher": {
+    "@type": "Organization",
+    "name": "3D Optika"
   }
-@endphp
-
-{!! json_encode($schema, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT) !!}
+}
 </script>
-@endif
